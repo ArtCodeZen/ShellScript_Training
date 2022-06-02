460dd30b671128828ecfe9ddbdd450917cc8d0b9
artcodezen <henrique.watpequim@gmail.com>
2022-06-02 09:45:25 -0300

    new scripts input, variables ...

---
+++ Scripts/input_text.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#one argument
+read -p "Enter your name:" name
+echo "Hi, $name. Let us be friends!"
+
+
+#more than one argument
+read -p "Enter first, last name:" first_name last_name
+echo "Name: $first_name. Last_name: $last_name"
+
